d5a1b76a1d6f34481f9075ba1e31fe4ebf535b08
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-06-24 21:35:36 +0300

    tests passed

--- server/app.py
+++ server/app.py
@@ -8,5 +8,30 @@ app = Flask(__name__)
 def index():
     return '<h1>Python Operations with Flask Routing and Views</h1>'
 
+@app.route('/print/<parameter>')
+def print_string(parameter):
+    print(parameter)
+    return parameter
+
+@app.route('/count/<parameter>')
+def count(parameter):
+    nums = ''
+    for i in range(int(parameter)):
+        nums += f'{i}\n'
+    return nums
+
+@app.route('/math/<num1>/<operation>/<num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        return f'{int(num1) + int(num2)}'
+    elif operation == '-':
+        return f'{int(num1) - int(num2)}'
+    elif operation == '*':
+        return f'{int(num1) * int(num2)}'
+    elif operation == 'div':
+        return f'{int(num1) / int(num2)}'
+    elif operation == '%':
+        return f'{int(num1) % int(num2)}'
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
